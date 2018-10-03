e57407b86a85ba0f7a032e4f21bf1bcf9dda986a
thimarcal <thiago.gm.pereira@gmail.com>
2018-10-03 00:02:44 -0300

    Sem dicionario

--- Tarefa01.py
+++ Tarefa01.py
@@ -1,9 +1,6 @@
 import os, time, string
-from multiprocessing import Process
-from multiprocessing import Manager
-from multiprocessing import Pool
 
-os.chdir('/Users/thiagom/Documents/Studies/Unicamp/MDC/INF-617/Tarefas/INF0617_Tarefa1')
+os.chdir('D:\\Thiago\\Studies\\Unicamp\\MDC\\INF-617\\Tarefas\\INF0617_Tarefa1')
 #print (os.getcwd())
 
 filenames = []
@@ -12,9 +9,9 @@ for file in os.listdir('./txt'):
         filenames.append(file)
 
 CHARACTERS = list(string.ascii_lowercase) + list(range(0,10))
-# print (CHARACTERS)
+
         
-def processar(filename, result): 
+def processar(filename):
     f = open('./txt/'+filename, encoding='utf-8', errors='ignore')
     contents = f.read().lower()
     f.close()
@@ -22,59 +19,65 @@ def processar(filename, result):
     total = []
     for position, character in enumerate(CHARACTERS):
         total.append(contents.count(str(character)))
-        #print ("Letra: ", character, "contagem:", total[position])
-    result[filename] = CHARACTERS[total.index(max(total))]
-    #print ("File:", filename, "Letter", CHARACTERS[total.index(max(total))])
+
+    print ("File:", filename, "Letter", CHARACTERS[total.index(max(total))])
 
 """
     Processamento Serial
 """
-manager = Manager()
-resultado_serial = manager.dict()
-start_time = time.time()
-for filename in filenames:
-    processar(filename, resultado_serial)
-end_time = time.time()
-print("Processamento serial", end_time-start_time)
-print(resultado_serial)
+def proc_serial():
+
+    start_time = time.time()
+    for filename in filenames:
+        processar(filename)
+    end_time = time.time()
+    print("Processamento serial", end_time-start_time)
 
 
 """
     Processamento Paralelo
 """
-processes = []
-resultado_paralelo = manager.dict()
-start_time = time.time()
-for filename in filenames:
-    p = Process(target=processar, args=(filename,resultado_paralelo))
-    processes.append(p)
-    p.start()
-    
-for p in processes:
-    p.join()
-end_time = time.time()
-print("Processamento paralelo", end_time-start_time)
-print(resultado_paralelo)
+def proc_paralelo():
+    processes = []
+    start_time = time.time()
+    for filename in filenames:
+        p = Process(target=processar, args=(filename,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+    end_time = time.time()
+    print("Processamento paralelo", end_time-start_time)
 
 
-result_pool = manager.dict()
-def processar_pool(filename): 
+def processar_pool(filename):
     f = open('./txt/'+filename, encoding='utf-8', errors='ignore')
     contents = f.read().lower()
     f.close()
-    
+
     total = []
     for position, character in enumerate(CHARACTERS):
         total.append(contents.count(str(character)))
-        #print ("Letra: ", character, "contagem:", total[position])
-    result_pool[filename] = CHARACTERS[total.index(max(total))]
+    print("File", filename, "Max char", CHARACTERS[total.index(max(total))])
 
-start_time = time.time()   
-pool = Pool(processes=16)
-pool.map(processar_pool, filenames)
-pool.close()
-pool.join()
-end_time = time.time()
 
-print("Processamento pool", end_time-start_time)
-print(result_pool)
+def proc_pool():
+    start_time = time.time()
+    pool = Pool(processes=16)
+    pool.map(processar_pool, filenames)
+    pool.close()
+    pool.join()
+    end_time = time.time()
+
+    print("Processamento pool", end_time-start_time)
+
+
+if __name__ == '__main__':
+    from multiprocessing import Process
+    from multiprocessing import Pool
+
+    proc_serial()
+    proc_paralelo()
+    proc_pool()
+
